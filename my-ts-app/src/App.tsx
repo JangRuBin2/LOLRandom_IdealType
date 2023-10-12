702ce405ed805b3648fbf7a48bcea6cd8e832bed
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-10-12 15:53:39 +0900

    axios설치 및 챔피언 정보 받아오기 확인

--- my-ts-app/src/App.tsx
+++ my-ts-app/src/App.tsx
@@ -1,9 +1,20 @@
+import axios from 'axios';
 import './App.css';
-
 function App() {
+  function findSpouse() {
+    axios.get('https://ddragon.leagueoflegends.com/cdn/12.6.1/data/ko_KR/champion.json')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+  };
   return (
+    <div>
     <h2>당신의 미래 배우자는?</h2>
-    
+    <button onClick={findSpouse}>지금 확인</button>
+    </div>
   );
 }
 

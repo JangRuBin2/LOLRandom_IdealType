ee8c291924aaf2d8725f4cda04fee5148725e569
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-10-22 11:38:33 +0900

    atom 데이터가 아닌 response 데이터로 이미지 태그 생성하여 버그 해결

--- my-ts-app/src/getMarrige.tsx
+++ my-ts-app/src/getMarrige.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { maritalStatus, partnerImgValue, useSetMarriage, useSetPartnerImg, useSetPartnerState, userPatnerValue } from "./recoil/champion";
+import { maritalStatus, partnerImgValue, useSetMarriage, useSetPartnerImg, useSetPartnerState, useSetuserPartner, userPartnerStatus, userPatnerValue } from "./recoil/champion";
 
 interface ChampionData {
   version: string;
@@ -13,6 +12,10 @@ interface ChampionData {
 }
 
 const GetMarrige = (): JSX.Element => {
+  // 파트너 상태 관리
+  const [isPaired, setPaired] = useRecoilState(userPartnerStatus);
+  // 파트너 상태 관리 함수
+  const setpairing = useSetuserPartner();
   // 결혼 상태
   const [isMarried, setMarried] = useRecoilState(maritalStatus);
   // 결혼 상태 관리 함수
@@ -25,9 +28,8 @@ const GetMarrige = (): JSX.Element => {
   const userPartnerImg : ChampionData | any = useRecoilValue(partnerImgValue);
   // 파트너 이미지 갱신 함수
   const setPartnerImg = useSetPartnerImg();
-  // 이미지 로드 상태를 관리할 상태 추가
-  const [imageLoaded, setImageLoaded] = useState(false);
 
+  // 랜덤 챔피언 정보 받아오는 함수
   function getRandomChampion(championData: any | unknown) {
     const championsArray = Object.values(championData.data); // 챔피언 데이터 배열로 변환
     const randomChampion = championsArray[Math.floor(Math.random() * championsArray.length)];
@@ -42,8 +44,7 @@ const GetMarrige = (): JSX.Element => {
   async function getPartnerImg(name : string) {
     try {
       console.log('인자가 잘 들어오는지 확인',name);
-      await axios.get(`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${name}_0.jpg`, { responseType: "blob" })
-  .then(response => {
+      const response = await axios.get(`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${name}_0.jpg`, { responseType: "blob" })
     const reader = new FileReader();
     reader.readAsDataURL(response.data);
     reader.onload = () => {
@@ -51,8 +52,8 @@ const GetMarrige = (): JSX.Element => {
       setPartnerImg(reader.result);
     };
     // atom 확인
-    console.log(userPartnerImg);
-  })}
+    // console.log(userPartnerImg);
+  }
     catch (error) {
       console.log('이미지 가져오기 실패', error);
     }
@@ -62,7 +63,9 @@ const GetMarrige = (): JSX.Element => {
     // 여기에 빠꾸하는 로직을 추가
     console.log("Atom에 저장된 파트너 데이터:", userPartnerData);
     try {
-    // 현재 파트너 정보 초기화
+      // 파트너 없는 상태
+      setpairing(false);
+      // 현재 파트너 정보 초기화
       setPartnerState(null);
       // 다시 챔피언 정보 받아옴
       findPartner();
@@ -74,36 +77,34 @@ const GetMarrige = (): JSX.Element => {
   // 소개팅 함수
   async function findPartner() {
     try {
-      await axios
-        .get('https://ddragon.leagueoflegends.com/cdn/12.6.1/data/ko_KR/champion.json')
-        .then((response) => {
-          // API에서 받아온 챔피언 데이터
-          const partnerData = response.data;
-          // 랜덤 챔피언 선택
-          const randomChampion = getRandomChampion(partnerData);
-          // 파트너 데이터 업데이트
-          setPartnerState(randomChampion);
-          console.log('이 상대는 어떠신가요?', randomChampion);
-          // 챔피언 이미지 데이터 받아오는 함수
-          getPartnerImg(userPartnerData.id);
-          console.log(typeof(userPartnerData.id));
-        });
+      const response = await axios.get('https://ddragon.leagueoflegends.com/cdn/12.6.1/data/ko_KR/champion.json');
+      // API에서 받아온 챔피언 데이터
+      const partnerData = response.data;
+      // 랜덤 챔피언 선택
+      const randomChampionData : ChampionData | any = getRandomChampion(partnerData);
+      // 파트너 데이터 업데이트
+      setPartnerState(randomChampionData);
+      console.log('최초에 받아온 랜덤 챔피언 데이터', randomChampionData);
+      // 파트너가 있는 상태
+      setpairing(true);
+  
+      // 챔피언 id로 이미지 데이터 받아오는 함수 실행
+      if (randomChampionData) {
+        console.log('이 상대는 어떠신가요?', randomChampionData);
+        // 여기서 getPartnerImg를 호출하도록 변경
+        getPartnerImg(randomChampionData.id);
+        console.log(typeof randomChampionData.id);
+      }
     } catch (error) {
       console.log('결혼 상대 매칭 실패', error);
     }
   }
+  
   // 결혼 취소
   function divorce() {
     setMarried(false);
   }
-  // 이미지 로드가 완료된 후 실행되는 효과
-  useEffect(() => {
-    if (userPartnerImg) {
-      setImageLoaded(true);
-      console.log('이미지 상태 업데이트', userPartnerImg);
-    }
-  }, [userPartnerImg]);
-  // 결혼 상태의 태그
+  // 결혼 상태일 때 태그
   if (isMarried) {
     return (<><p>{userPartnerData.name}와의 결혼을 축하합니다!</p>
     <button onClick={divorce}>파혼</button>
@@ -119,19 +120,18 @@ const GetMarrige = (): JSX.Element => {
   }
   return (
     <div>
+    {userPartnerData ? (<>
       <h4>{userPartnerData.title}</h4>
       <h2>{userPartnerData.name}</h2>
       <p>{userPartnerData.blurb}</p>
-      {imageLoaded ? ( // 이미지가 로드된 후에만 이미지를 화면에 표시
+    </>) : (<p>챔피언 정보 기다리는 중...</p>)}
         <img
           src={userPartnerImg}
           alt="파트너 이미지"
-          width="100"
-          height="100"
+          width="300"
+          height="400"
         />
-      ) : (
-        <p>이미지 로딩 중...</p>
-      )}
+      
       <button onClick={marryMe}>결혼해주세요</button>
       <button onClick={cancelDate}>당신은 내 스타일이 아녜요</button>
     </div>

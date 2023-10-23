70db21ea2176988b91a90d272617a43bae44fb4b
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-10-23 14:24:47 +0900

    리액트 라우터 적용

--- my-ts-app/src/App.tsx
+++ my-ts-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import GetMarrige from './getMarrige';
 function App() {
@@ -5,7 +6,13 @@ function App() {
     <div className='main_container'>
       <h2>LoLIdeal에서 당신의 파트너를 찾아보세요!</h2>
         <div className='main_box'>
-          <GetMarrige />
+          <Router>
+            <Routes>
+              <Route path='/' Component={GetMarrige}>
+              </Route>
+              <Route path='/test' Component={GetMarrige}></Route>
+            </Routes>
+          </Router>
         </div>
     </div>
   );

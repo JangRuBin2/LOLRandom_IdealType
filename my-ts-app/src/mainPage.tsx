916f435910d3297dba753ea7f43e7435f979d7b4
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-10-25 14:14:42 +0900

    파일 내부에서 css 변수 선언 후 사용

--- my-ts-app/src/mainPage.tsx
+++ my-ts-app/src/mainPage.tsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+// css
+const content_btn_style : React.CSSProperties= {
+  display : "flex", flexDirection : "row", justifyContent : "space-between", alignItems : "center", backgroundColor : '#D9D9D9', width : '300px', height : '130px'
+}
+// const btn_div_style : content_btn_style = {}
 const MainPage = () : JSX.Element => {
-  return (<div style={{
-    display : "flex", flexDirection : "row", justifyContent : "space-between", alignItems : "center"
-  }}>
-    <Link to='/randomPick'>
-    <div>랜덤 뽑기</div>
-    </Link>
-    <Link to='/worldCup'>
-    <div>이상형 월드컵</div>
-    </Link>
-    <Link to='exList'>
-    <div>전여친 목록</div>
-    </Link>
+  return (<div style={content_btn_style}>
+    <div>
+    <Link to='/randomPick'>랜덤뽑기</Link>
+    </div>
+    <div>
+    <Link to='/worldCup'>이상형 월드컵</Link>
+    </div>
+    <div>
+    <Link to='/exList'>전여친 목록</Link>
+    </div>
   </div>)
 };
 export default MainPage;

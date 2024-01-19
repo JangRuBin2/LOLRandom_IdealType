b151ba5cc273b083e9618f72093f76639f200b18
JangRuBin2 <wkdfnqls2465@gmail.com>
2024-01-19 17:23:51 +0900

    무한루프 에러 해결완료 -> 조건문에 의한 setState()동시 실행

--- my-ts-app/src/worldCup.tsx
+++ my-ts-app/src/worldCup.tsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { ChampionData } from './recoil/champion';
+
 type selectedRoundround = {
-  round : string
+  round: string;
 };
-const WorldCup : React.FC<selectedRoundround>= ({round}) : JSX.Element => {
+
+const WorldCup: React.FC<selectedRoundround> = ({ round }): JSX.Element => {
   // 토너먼트를 진행할 챔피언 정보 저장
-  const [worldCupMembers, setWorldCupMembes] = useState<ChampionData[]>([]);
+  const [worldCupMembers, setWorldCupMembers] = useState<ChampionData[]>([]);
   const [winMembers, setWinMembers] = useState<ChampionData[]>([]);
   // 토너먼트 라운드 state
   const [currentMatch, setCurrentMatch] = useState<number>(0);
@@ -23,32 +25,36 @@ const WorldCup : React.FC<selectedRoundround>= ({round}) : JSX.Element => {
     console.log('randomChampions : ', randomChampions);
     return randomChampions;
   };
-   // 챔피언 정보 가져오는 함수
-  async function findPartner() {
-    try {
-      const response = await axios.get('https://ddragon.leagueoflegends.com/cdn/12.6.1/data/ko_KR/champion.json');
-        const allChampionData = response.data;
-        const randomChampionData : ChampionData | any = getRandomChampion(allChampionData);
-        setWorldCupMembes(randomChampionData);
-        console.log(randomChampionData);
-    } catch (error) {
-      console.log('챔피언 데이터 가져오기 실패', error);
-    }}
   // 챔피언 정보 가져오는 함수
-  useEffect(()=> {
+  useEffect(() => {
+    // 챔피언 정보 가져오는 함수
+    const findPartner = async () => {
+      try {
+        const response = await axios.get('https://ddragon.leagueoflegends.com/cdn/12.6.1/data/ko_KR/champion.json');
+        const allChampionData = response.data;
+        const randomChampionData: ChampionData | any = getRandomChampion(allChampionData);
+        setWorldCupMembers(randomChampionData);
+        console.log('randomChampionData : ',randomChampionData);
+      } catch (error) {
+        console.log('챔피언 데이터 가져오기 실패', error);
+      }
+    };
+
     findPartner();
-  }, [round]);
+  }, []);
+
   // 둘 다 선택하기 싫을 때
   const chooseNone = () => {
+    
     setCurrentMatch((prevMatch) => prevMatch + 1);
   };
-  
-  const championSelect = (champion: ChampionData) : void => {
+
+  const championSelect = (champion: ChampionData): void => {
     setWinMembers((prevSelectedChampions) => [...prevSelectedChampions, champion]);
     setCurrentMatch((prevMatch) => prevMatch + 1);
   };
 
-// 매칭 된 상대를 랜더링하는 컴포넌트
+  // 매칭 된 상대를 랜더링하는 컴포넌트
   const renderMatchups = () => {
     const matchups: JSX.Element[] = [];
 
@@ -60,27 +66,41 @@ const WorldCup : React.FC<selectedRoundround>= ({round}) : JSX.Element => {
       matchups.push(
         <div key={`matchup-${i}`}>
           <div onClick={() => championSelect(champion1)}>
-          {imageArray[i] && <img src={imageArray[i]} alt="챔피언 이미지"/>}
-          <h2>{champion1.id}</h2>
+            {imageArray[i] && <img src={imageArray[i]} alt="챔피언 이미지" />}
+            <h2>{champion1.id}</h2>
           </div>
           VS
           <div onClick={() => championSelect(champion2)}>
-          {imageArray[i+1] && <img src={imageArray[i+1]} alt="챔피언 이미지" />}
-          <h2>{champion2.id}</h2>
+            {imageArray[i + 1] && <img src={imageArray[i + 1]} alt="챔피언 이미지" />}
+            <h2>{champion2.id}</h2>
           </div>
         </div>
       );
     }
-    if (currentMatch * 2 >= worldCupMembers.length) {
+    console.log('winMembers.length : ', winMembers.length);
+    console.log('currentMatch : ', currentMatch);
+    console.log('worldCupMembers.length : ', worldCupMembers.length);
+    if (currentMatch * 2 === worldCupMembers.length) {
+      console.log('경기 끝?')
       // 다른 페이지로 이동하는 함수 호출 또는 다른 로직을 추가하세요.
+      if (winMembers.length === parseInt(round, 10) / 2) {
+        setWorldCupMembers(winMembers);
+        console.log('정상 실행됨?');
+      //   setWinMembers([]);
+      //   setCurrentMatch(0);
+      // } else {
+      //   console.log('토너먼트 종료');
+      //   // 토너먼트가 완전히 종료된 경우, 여기에 추가 로직을 넣을 수 있습니다.
+      }
       console.log('경기 끝');
     }
     return matchups;
   };
-// 챔피언 이미지 불러오는 함수
+
+  // 챔피언 이미지 불러오는 함수
   useEffect(() => {
     const fetchImages = async () => {
-      const promises = worldCupMembers.map(async (item : any) => {
+      const promises = worldCupMembers.map(async (item: any) => {
         try {
           const response = await axios.get(
             `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${item.id}_0.jpg`,
@@ -102,15 +122,22 @@ const WorldCup : React.FC<selectedRoundround>= ({round}) : JSX.Element => {
       const results = await Promise.all(promises);
       setImageArray(results);
     };
+
     fetchImages();
   }, [worldCupMembers]);
-  
-  return (<>
-  <h2>{round}강</h2>
-  {renderMatchups()}
-  {currentMatch < worldCupMembers.length / 2 && (
+
+  return (
+    <>
+      <h2>{round}강</h2>
+      {
+        
+      }
+      {renderMatchups()}
+      {currentMatch < worldCupMembers.length / 2 && (
         <button onClick={chooseNone}>기권</button>
       )}
-  </>)
-}
+    </>
+  );
+};
+
 export default WorldCup;
